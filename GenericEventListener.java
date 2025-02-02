147c0a5fc8a548dd2a5f6f34b7a83e0a0ed019f1
Stanislav Kuznets <sy.kuznets@gmail.com>
2025-02-02 17:28:56 +0300

    Enhance code formatting

--- GenericEventListener.java
+++ GenericEventListener.java
@@ -10,23 +10,24 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @KafkaListener(
-    topics = {"${app.kafka.consumer.topics.event.name}"},
-    groupId = "${app.kafka.consumer.group-id}",
-    containerFactory = "kafkaListenerEvent"
+        topics = {"${app.kafka.consumer.topics.event.name}"},
+        groupId = "${app.kafka.consumer.group-id}",
+        containerFactory = "kafkaListenerEvent"
 )
 @Slf4j
 public class GenericEventListener {
 
-  private final GenericOperation operation;
-  private final EventListenerMapper eventMapper;
+    private final GenericOperation operation;
+    private final EventListenerMapper eventMapper;
 
-  @KafkaHandler
-  public void handleEvent(GenericEvent event) {
-    log.info("Received GenericEvent: '{}'", event);
+    @KafkaHandler
+    public void handleEvent(GenericEvent event) {
+        log.info("Received GenericEvent: '{}'", event);
 
-    var operationType = eventMapper.toDomain(event.getEventData().getOperationType());
-    var entityId = event.getEventPayload().getId();
+        var operationType = eventMapper.toDomain(event.getEventData().getOperationType());
+        var entityId = event.getEventPayload().getId();
 
-    operation.execute(entityId, operationType);
-  }
+        operation.execute(entityId, operationType);
+    }
+
 }
