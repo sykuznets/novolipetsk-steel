dd77a63d4e2f8eb4ba2d68acee3a07b46e2f7439
Stanislav Kuznets <sy.kuznets@gmail.com>
2025-02-02 12:45:56 +0300

    Add files via upload

---
+++ ItemRepository.java
@@ -0,0 +1,128 @@
+package adapters.repositories;
+
+import adapters.repositories.records.Record;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ItemRepository extends JpaRepository<Record, Long>, JpaSpecificationExecutor<Record> {
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT i.*
+            FROM items i
+            WHERE (
+                LOWER(CAST(i.roles AS varchar)) LIKE '%user%'
+                OR i.roles IS NULL
+            )
+            AND (
+                i.codes IS NULL
+                OR EXISTS (
+                    SELECT 1
+                    FROM UNNEST(i.codes) AS code
+                    WHERE code IN (:codes)
+                )
+            )
+            ORDER BY i.priority, i.created DESC
+            LIMIT 10;
+        """
+    )
+    List<Record> findItemsForUser(@Param("codes") List<Integer> codes);
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT i.*
+            FROM items i
+            WHERE (
+                LOWER(CAST(i.roles AS varchar)) LIKE '%user%'
+                OR i.roles IS NULL
+            )
+            AND i.codes IS NULL
+            ORDER BY i.priority, i.created DESC
+            LIMIT 10;
+        """
+    )
+    List<Record> findItemsForUserWithNoCodes();
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT i.*
+            FROM items i
+            WHERE (
+                LOWER(CAST(i.roles AS varchar)) LIKE '%admin%'
+                OR i.roles IS NULL
+            )
+            AND (
+                i.codes IS NULL
+                OR EXISTS (
+                    SELECT 1
+                    FROM UNNEST(i.codes) AS code
+                    WHERE code IN (:codes)
+                )
+            )
+            ORDER BY i.priority, i.created DESC
+            LIMIT 10;
+            """
+    )
+    List<Record> findItemsForAdmin(@Param("codes") List<Integer> codes);
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT i.*
+            FROM items i
+            WHERE (
+                LOWER(CAST(i.roles AS varchar)) LIKE '%admin%'
+                OR i.roles IS NULL
+            )
+            AND i.codes IS NULL
+            ORDER BY i.priority, i.created DESC
+            LIMIT 10;
+        """
+    )
+    List<Record> findItemsForAdminWithNoCodes();
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT i.*
+            FROM items i
+            WHERE (
+                LOWER(CAST(i.roles AS varchar)) LIKE '%staff%'
+                OR i.roles IS NULL
+            )
+            AND (
+                i.codes IS NULL
+                OR EXISTS (
+                    SELECT 1
+                    FROM UNNEST(i.codes) AS code
+                    WHERE code IN (:codes)
+                )
+            )
+            ORDER BY i.priority, i.created DESC
+            LIMIT 10;
+        """
+    )
+    List<Record> findItemsForStaff(@Param("codes") List<Integer> codes);
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT i.*
+            FROM items i
+            WHERE (
+                LOWER(CAST(i.roles AS varchar)) LIKE '%staff%'
+                OR i.roles IS NULL
+            )
+            AND i.codes IS NULL
+            ORDER BY i.priority, i.created DESC
+            LIMIT 10;
+        """
+    )
+    List<Record> findItemsForStaffWithNoCodes();
+}
