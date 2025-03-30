cf655d15f872c127ef0c4f6a48d6c6ee9fbd38ba
Stanislav Kuznets <sy.kuznets@gmail.com>
2025-03-30 18:34:44 +0300

    Improve code formatting

--- ItemRepository.java
+++ ItemRepository.java
@@ -7,72 +7,80 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Record, Long>, JpaSpecificationExecutor<Record> {
+public interface ItemRepository
+        extends JpaRepository<Record, Long>, JpaSpecificationExecutor<Record> {
 
     @Query(nativeQuery = true, value = """
-                SELECT i.*
-                FROM items i
-                WHERE (i.roles ILIKE '%user%' OR i.roles IS NULL)
-                    AND (i.codes IS NULL OR EXISTS (
-                        SELECT 1 FROM UNNEST(i.codes) AS code WHERE code IN (:codes)
-                    ))
-                ORDER BY i.priority, i.created DESC
-                LIMIT 10
+        SELECT i.*
+        FROM items i
+        WHERE (i.roles ILIKE '%user%' OR i.roles IS NULL)
+            AND (i.codes IS NULL OR EXISTS (
+                SELECT 1 FROM UNNEST(i.codes) AS code
+                WHERE code IN (:codes)
+            ))
+        ORDER BY i.priority, i.created DESC
+        LIMIT 10
     """)
     List<Record> findItemsForUser(@Param("codes") List<Integer> codes);
 
+
     @Query(nativeQuery = true, value = """
-                SELECT i.*
-                FROM items i
-                WHERE (i.roles ILIKE '%user%' OR i.roles IS NULL)
-                    AND i.codes IS NULL
-                ORDER BY i.priority, i.created DESC
-                LIMIT 10
+        SELECT i.*
+        FROM items i
+        WHERE (i.roles ILIKE '%user%' OR i.roles IS NULL)
+            AND i.codes IS NULL
+        ORDER BY i.priority, i.created DESC
+        LIMIT 10
     """)
     List<Record> findItemsForUserWithNoCodes();
 
+
     @Query(nativeQuery = true, value = """
-                SELECT i.*
-                FROM items i
-                WHERE (i.roles ILIKE '%admin%' OR i.roles IS NULL)
-                    AND (i.codes IS NULL OR EXISTS (
-                        SELECT 1 FROM UNNEST(i.codes) AS code WHERE code IN (:codes)
-                    ))
-                ORDER BY i.priority, i.created DESC
-                LIMIT 10
+        SELECT i.*
+        FROM items i
+        WHERE (i.roles ILIKE '%admin%' OR i.roles IS NULL)
+            AND (i.codes IS NULL OR EXISTS (
+                SELECT 1 FROM UNNEST(i.codes) AS code
+                WHERE code IN (:codes)
+            ))
+        ORDER BY i.priority, i.created DESC
+        LIMIT 10
     """)
     List<Record> findItemsForAdmin(@Param("codes") List<Integer> codes);
 
+
     @Query(nativeQuery = true, value = """
-                SELECT i.*
-                FROM items i
-                WHERE (i.roles ILIKE '%admin%' OR i.roles IS NULL)
-                    AND i.codes IS NULL
-                ORDER BY i.priority, i.created DESC
-                LIMIT 10
+        SELECT i.*
+        FROM items i
+        WHERE (i.roles ILIKE '%admin%' OR i.roles IS NULL)
+            AND i.codes IS NULL
+        ORDER BY i.priority, i.created DESC
+        LIMIT 10
     """)
     List<Record> findItemsForAdminWithNoCodes();
 
+
     @Query(nativeQuery = true, value = """
-                SELECT i.*
-                FROM items i
-                WHERE (i.roles ILIKE '%staff%' OR i.roles IS NULL)
-                    AND (i.codes IS NULL OR EXISTS (
-                        SELECT 1 FROM UNNEST(i.codes) AS code WHERE code IN (:codes)
-                    ))
-                ORDER BY i.priority, i.created DESC
-                LIMIT 10
+        SELECT i.*
+        FROM items i
+        WHERE (i.roles ILIKE '%staff%' OR i.roles IS NULL)
+            AND (i.codes IS NULL OR EXISTS (
+                SELECT 1 FROM UNNEST(i.codes) AS code
+                WHERE code IN (:codes)
+            ))
+        ORDER BY i.priority, i.created DESC
+        LIMIT 10
     """)
     List<Record> findItemsForStaff(@Param("codes") List<Integer> codes);
 
+
     @Query(nativeQuery = true, value = """
-                SELECT i.*
-                FROM items i
-                WHERE (i.roles ILIKE '%staff%' OR i.roles IS NULL)
-                    AND i.codes IS NULL
-                ORDER BY i.priority, i.created DESC
-                LIMIT 10
+        SELECT i.*
+        FROM items i
+        WHERE (i.roles ILIKE '%staff%' OR i.roles IS NULL)
+            AND i.codes IS NULL
+        ORDER BY i.priority, i.created DESC
+        LIMIT 10
     """)
     List<Record> findItemsForStaffWithNoCodes();
-
 }
