f25e73f7c5d1594933147c27593cb8d2ab683f29
Stanislav Kuznets <sy.kuznets@gmail.com>
2025-04-10 13:33:13 +0300

    Create update-workflow-actions-and-job-scopes.sql

---
+++ update-workflow-actions-and-job-scopes.sql
@@ -0,0 +1,27 @@
+-- File: update_workflow_actions_and_job_scopes.sql
+-- liquibase formatted SQL
+-- changeset kuznets_sy:134
+
+-- Modify the 'action_code' column in the workflow_actions table.
+ALTER TABLE workflow_actions ALTER COLUMN action_code TYPE VARCHAR(64);
+
+-- Insert or update a set of actions into the workflow_actions table.
+INSERT INTO workflow_actions (action_code, action_name) VALUES
+    ('actualize', 'Actualizes'),
+    ('analyze', 'Analyzes'),
+    ('execute', 'Executes'),
+    ('consult', 'Consults'),
+    ('control', 'Controls'),
+    ('coordinate', 'Coordinates'),
+    ('provide', 'Provides'),
+    ('organize', 'Organizes'),
+    ('implement', 'Implements')
+ON CONFLICT (action_name)
+DO UPDATE SET action_code = EXCLUDED.action_code;
+
+-- Insert a set of job scopes.
+INSERT INTO job_scopes (scope_code, scope_name) VALUES
+    ('enterprise', 'Enterprise'),
+    ('department', 'Department'),
+    ('division', 'Division'),
+    ('direction', 'Section');
